7bd91b36d3ba840f14f0ad7587f88a9ad6695cc9
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2024-08-05 17:43:01 +0200

    Unused Button import removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { AppShell, Button, NavLink } from '@mantine/core';
+import { AppShell, NavLink } from '@mantine/core';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
